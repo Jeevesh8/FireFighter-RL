dca778eb9b911929cbe47ea03f5973f0e2c1a4f9
jeevesh8 <creativityinczenyoga@gmail.com>
2021-03-28 20:41:51 +0530

    use nonlocal with nested functions

--- dp.py
+++ dp.py
@@ -14,6 +14,7 @@ def get_all_states(env, agent):
     states = []
     
     def get_all_states(env, agent, timestep):
+        nonlocal states
         begin_state = timestep.observation[1:]
         if begin_state not in states:
             states.append(begin_state)
