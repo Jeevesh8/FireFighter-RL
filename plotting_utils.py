93562188f75ecdf1b8bdeab4a8b9880e98a5ee45
jeevesh8 <creativityinczenyoga@gmail.com>
2021-05-20 17:52:59 +0530

    chissana hosei

--- plotting_utils.py
+++ plotting_utils.py
@@ -6,6 +6,6 @@ def visualize_graph(adj_mat, burned, defended):
     g = nx.convert_matrix.from_numpy_matrix(adj_mat)
     node_color = np.where(burned, 1.0, 0.2)
     node_color = np.where(defended, 0.6, node_color)
-    nx.draw_planar(g, node_color=node_color.tolist())
+    nx.draw_spectral(g, node_color=node_color.tolist())
     plt.plot()
     plt.show()
